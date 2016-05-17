027d8065335dce43f010bf24715a6acb96776f45
FelixGSE <felix@Felixs-MacBook-Pro.local>
2016-05-18 00:33:58 +0200

    Push Problem Set 2 Final

---
+++ push.sh
@@ -0,0 +1,6 @@
+echo "I'm Pushing to github"
+
+cd "/Users/felix/Documents/GSE/Term 3/14D009 Social and Economic Networks/Problemsets/14D009-Social-and-Economic-Networks/"
+git add -A
+git commit -m "$1"
+git push origin master
